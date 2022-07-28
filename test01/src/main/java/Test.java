a4553ccd8dc7c46ad9da3eaef75cfb13740303c0
ys <s__yang@163.com>
2022-07-28 18:39:48 +0800

    master 001

--- test01/src/main/java/Test.java
+++ test01/src/main/java/Test.java
@@ -2,11 +2,11 @@
  * @Package: PACKAGE_NAME
  * @ClassName: Test
  * @Author: YS
- * @CreateTime: 2022/7/28 18:36
+ * @CreateTime: 2022/7/28 18:38
  * @Description:
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hot-fix");
+        System.out.println("master");
     }
 }
