d532423eabd966d3ea3acbb0a8c86a6b7070e70b
ys <s__yang@163.com>
2022-07-28 18:38:19 +0800

    hot-fix1

---
+++ test01/src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @Package: PACKAGE_NAME
+ * @ClassName: Test
+ * @Author: YS
+ * @CreateTime: 2022/7/28 18:36
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hot-fix");
+    }
+}
